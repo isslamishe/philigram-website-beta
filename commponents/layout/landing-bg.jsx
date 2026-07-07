7b11c8ae580346994ab7ef13de595cefcc38cfc4
isslamishe <isslamxr8@gmail.com>
2026-07-07 16:03:37 +0100

    fixing landing page bg on mobile

--- commponents/layout/landing-bg.jsx
+++ commponents/layout/landing-bg.jsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 export default function HeroBg() {
   return (
-    <div className="flex pointer-events-none brightness-75 bg-fixed bg-[url('/home/section-1-bg.svg')] bg-no-repeat bg-cover bg-center z-0 flex-col absolute top-0 flex-1 overflow-hidden  w-dvw h-dvh items-center   justify-center ">
+    <div className="flex pointer-events-none brightness-75 fixed bg-no-repeat bg-cover bg-center z-0 flex-col  top-0 flex-1 overflow-hidden  w-dvw h-dvh items-center   justify-center ">
+      <Image
+        fill
+        alt=""
+        className="object-center"
+        src={"/home/section-1-bg.svg"}
+      />
       <div className="bg-gradient-to-b z-30 absolute opacity-100 h-full w-full from-transparent to-[#101F3F]" />
     </div>
   );
