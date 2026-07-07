686cb2aef91b4722d6204269f63fa6ff96b24c8f
isslamishe <isslamxr8@gmail.com>
2026-07-07 16:25:16 +0100

    fixing switch buttons on mobile

--- commponents/sections/races/race commponents/switch.buttons.jsx
+++ commponents/sections/races/race commponents/switch.buttons.jsx
@@ -24,14 +24,14 @@ export default function Races({ currentRace, races }) {
     <div className="flex  overflow-hidden  justify-around items-center flex-row  bg-black gap-0 bg-no-repeat bg-cover bg-center relative w-screen min-h-[30vh]">
       {currentIndex - 1 < 0 ? (
         <Link
-          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[15vh] flex justify-center items-center border border-primary"
+          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[10dvh] md:h-[15vh] flex justify-center items-center border border-primary"
           href={"/races"}
         >
           Retour aux Races
         </Link>
       ) : (
         <Link
-          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[15vh] flex justify-center items-center border border-primary"
+          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[10dvh] md:h-[15vh] flex justify-center items-center border border-primary"
           href={races[currentIndex - 1]?.diraction}
           onClick={() => {
             setCurrentSubNav(races[currentIndex - 1]?.name);
@@ -43,14 +43,14 @@ export default function Races({ currentRace, races }) {
 
       {currentIndex + 1 > races.length - 1 ? (
         <Link
-          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[15vh] flex justify-center items-center border border-primary"
+          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[10dvh] md:h-[15vh] flex justify-center items-center border border-primary"
           href={"/races"}
         >
           Retour aux Races
         </Link>
       ) : (
         <Link
-          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[15vh] flex justify-center items-center border border-primary"
+          className="w-[40%] hover:scale-110 transition-all duration-300 delay-100 uppercase font-serif72 text-primary text-center text-[clamp(0.6rem,2vw,1.2rem)]  md:text-[clamp(1rem,2vw,1.5rem)] h-[10dvh] md:h-[15vh] flex justify-center items-center border border-primary"
           href={races[currentIndex + 1]?.diraction}
           onClick={() => {
             setCurrentSubNav(races[currentIndex + 1]?.name);
